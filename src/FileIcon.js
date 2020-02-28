cd740b0162649507490276020aa8272bf699a37a
Adam Risberg <myniztan@hotmail.com>
2020-02-27 18:06:56 -0600

    Change icons to background images

--- src/FileIcon.js
+++ src/FileIcon.js
@@ -2,18 +2,18 @@ import React from "react";
 import styles from "./Window.module.css";
 
 function FileIcon({ name, imageSrc, onClick, windowId }) {
-  // Pass a callback from Windows to be used here for
-  // the onDrag event.
-
   return (
     <button
-      // onDrag={e => console.log(e.clientX, e.clientY)}
       className={styles.fileIconButton}
       onClick={onClick}
       data-window-id={windowId}
       aria-label={`Open ${name}`}
     >
-      <img src={imageSrc} alt="restore" data-window-id={windowId} />
+      <div
+        className={styles.fileIconImage}
+        style={{ backgroundImage: `url(${imageSrc})` }}
+        data-window-id={windowId}
+      />
       <span data-window-id={windowId}>{name}</span>
     </button>
   );
