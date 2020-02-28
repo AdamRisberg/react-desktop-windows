ba760192ddaedc9e542c3fc5c68b980296d41544
Adam Risberg <myniztan@hotmail.com>
2020-02-28 03:39:50 -0600

    Updates

---
+++ precache-manifest.729a29228690b7bed116ed1b76389a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30296e7629c240bb61f0081a451505fb",
+    "url": "/react-desktop-windows/index.html"
+  },
+  {
+    "revision": "c4300863c6034731df66",
+    "url": "/react-desktop-windows/static/css/main.420a01a1.chunk.css"
+  },
+  {
+    "revision": "42a756d037a39e922892",
+    "url": "/react-desktop-windows/static/js/2.6ff0a163.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-desktop-windows/static/js/2.6ff0a163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4300863c6034731df66",
+    "url": "/react-desktop-windows/static/js/main.1eed5b25.chunk.js"
+  },
+  {
+    "revision": "106c73f7ce782ca8b216",
+    "url": "/react-desktop-windows/static/js/runtime-main.01c10696.js"
+  }
+]);
